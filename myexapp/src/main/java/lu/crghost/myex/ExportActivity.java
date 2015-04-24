e1fdcc8476a8765406f97b4164e276ce979fe874
Charly <crghostapps@gmail.com>
2015-04-24 14:43:56 +0200

    ExportDB

--- myexapp/src/main/java/lu/crghost/myex/ExportActivity.java
+++ myexapp/src/main/java/lu/crghost/myex/ExportActivity.java
@@ -1,12 +1,24 @@
 package lu.crghost.myex;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import net.sqlcipher.database.SQLiteDatabase;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import lu.crghost.cralib.net.SshClient;
+import lu.crghost.myex.conf.MyExProperties;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 public class ExportActivity extends Activity {
@@ -15,12 +27,20 @@ public class ExportActivity extends Activity {
 
     private MyExApp app;
 
+    static class ViewHolder {
+        public ProgressBar progressBar;
+    }
+    ViewHolder holder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
         app = (MyExApp) getApplication();
+        holder = new ViewHolder();
+        holder.progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        holder.progressBar.setVisibility(View.GONE);
     }
 
 
@@ -52,14 +72,68 @@ public class ExportActivity extends Activity {
 
     public void action_export_backup(View v) {
 
-        // https://www.zetetic.net/sqlcipher/sqlcipher-api/#sqlcipher_export
-        SQLiteDatabase db = app.getDataManager().getDb();
-        String sql = "ATTACH DATABASE 'myexdbclean.db' AS myexdbclean KEY ''";
-        db.rawExecSQL(sql);
-        db.rawExecSQL("SELECT sqlcipher_export('myexdbclean')");
-        db.rawExecSQL("DETACH DATABASE myexdbclean");
+        LayoutInflater li = LayoutInflater.from(this);
+        View prompt = li.inflate(R.layout.prompt_backup, null);
+        final EditText txtbkserver = (EditText) prompt.findViewById(R.id.prompt_bkserver);
+        final EditText txtbkpass   = (EditText) prompt.findViewById(R.id.prompt_bkserverpass);
+        txtbkserver.setText(app.getPrefs().getString("pref_bkserver",""));
+        txtbkpass.setText(app.getPrefs().getString("pref_bkserverpass",""));
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setView(prompt);
+        dialog.setTitle(getResources().getString(R.string.export_backup));
+        dialog.setIcon(android.R.drawable.ic_dialog_dialer);
+        dialog.setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                holder.progressBar.setVisibility(View.VISIBLE);
+                URI uri = null;
+                try {
+                    uri = new URI(txtbkserver.getText().toString());
+                    DoBackup doBackup = new DoBackup(uri, txtbkpass.getText().toString());
+                    doBackup.execute("");
+                } catch (URISyntaxException e) {
+                    Toast.makeText(ExportActivity.this, getResources().getText(R.string.pref_bkserver_error), Toast.LENGTH_LONG).show();
+                }
 
-        Log.d(TAG, "Db recryped");
+            }
+        });
+        dialog.setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i(TAG,"Cancel pressed");
+            }
+        });
+        dialog.show();
 
     }
+
+
+    private class DoBackup extends AsyncTask<String,Void,String> {
+
+        URI bkserveruri;
+        String bkpass;
+
+        DoBackup(URI serveruri, String pass) {
+            bkserveruri = serveruri;
+            bkpass = pass;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            setProgress(1);
+            SshClient.putFiles(bkserveruri,bkpass,new String[] {MyExProperties.DATABASE_PATH});
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            holder.progressBar.setVisibility(View.GONE);
+            Toast.makeText(ExportActivity.this, getResources().getText(R.string.export_backup_end), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+
 }
--- myexapp/src/main/java/lu/crghost/myex/conf/MyExProperties.java
+++ myexapp/src/main/java/lu/crghost/myex/conf/MyExProperties.java
@@ -6,15 +6,17 @@ import android.content.pm.PackageManager;
 import android.os.Environment;
 
 /**
- * Created by CR on 24/12/2014.
+ * General constants
  */
 public final class MyExProperties {
 
     private static final String APP_PACKAGE_NAME = "lu.crghost.myex";
     public static final String  DATABASE_NAME = "myex.db" ;
+    public static final String  DATABASE_NAME_CLEAN = "myexclean.db" ;
     private static final String EXTERNAL_DATA_DIR_NAME = "myexdata";
     public static final String  EXTERNAL_DATA_PATH = Environment.getExternalStorageDirectory() + "/" + EXTERNAL_DATA_DIR_NAME;
     public static final String  DATABASE_PATH = Environment.getDataDirectory() + "/data/" + APP_PACKAGE_NAME + "/databases/" + DATABASE_NAME;
+    public static final String  DATABASE_PATH_CLEAN = Environment.getDataDirectory() + "/data/" + APP_PACKAGE_NAME + "/databases/" + DATABASE_NAME_CLEAN;
 
     public static String getVersionName(Context c) {
         String versionName = "Unknown version";
