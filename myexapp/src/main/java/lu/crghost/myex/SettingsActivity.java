901bb7b57a917a0d75ebad8c8f8e87f0880b9824
Charly <crghostapps@gmail.com>
2015-05-28 14:44:16 +0200

    Moving on

--- myexapp/src/main/java/lu/crghost/myex/SettingsActivity.java
+++ myexapp/src/main/java/lu/crghost/myex/SettingsActivity.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.util.Locale;
 
 /**
  * Created by CR on 14/04/2015.
  */
 public class SettingsActivity extends PreferenceActivity {
+
+    private static final String TAG = "SettingsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,14 @@ public class SettingsActivity extends PreferenceActivity {
         pmanager.setSharedPreferencesName(MyExApp.PREFS_FILENAME);
         pmanager.setSharedPreferencesMode(Context.MODE_MULTI_PROCESS);
         addPreferencesFromResource(R.xml.preferences);
+
+        Log.i(TAG,"Default locale: " + Locale.getDefault());
+        Log.i(TAG, "Available locales:");
+        Locale[] locales = Locale.getAvailableLocales();
+        for (int i=0; i < locales.length; i++) {
+            Log.i(TAG,locales[i].getLanguage() + " " + locales[i].getCountry());
+        }
+
     }
 
 }
